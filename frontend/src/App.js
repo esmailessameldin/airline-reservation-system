c5959635c9cf4c6770f54d47b1d5793f482cec22
esmail saba <somaasom3a@gmail.com>
2021-11-29 04:20:33 +0200

    ah

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import 'semantic-ui-css/semantic.min.css'
 import Adminhomepage from './components/AdminHomepage'
 import Homepage from './components/Homepage'
 import Navbar from './components/Navbar'
+import createflight from './components/create-flight'
 import AdminLogin from './components/Admin-Login.js'
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       
          <Route  path="/" exact component={Homepage}/>
          <Route  path="/admin" exact component={AdminLogin}/>
+         <Route  path="/:id/CreateFlight" exact component={createflight}/>
          <Route  path="/admin-homepage/:id" exact component={Adminhomepage}/>
 
     
