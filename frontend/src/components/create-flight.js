f3ae0c7949c971b943e919472a767193e500e4f1
esmail saba <somaasom3a@gmail.com>
2021-11-30 08:01:02 +0200

    frontend-create-flight

--- frontend/src/components/create-flight.js
+++ frontend/src/components/create-flight.js
@@ -5,15 +5,18 @@ import axios from 'axios';
 export default class CreateFlight extends Component {
   constructor(props) {
     super(props);
-
-    this.onChangeDay = this.onChangeDay.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onChangeTime=this.onChangeTime.bind(this)
-    this.onChangeCrn=this.onChangeCrn.bind(this)
-    this.onChangeBuilding=this.onChangeBuilding.bind(this)
-    this.onChangeRoom=this.onChangeRoom.bind(this)
-  
-
+    this.onChangeflightnumber= this.onChangeflightnumber.bind(this);
+    this.onChangefirstclass = this.onChangefirstclass.bind(this);
+    this.onChangeeconomy = this.onChangeeconomy.bind(this);
+    this.onChangebusiness = this.onChangebusiness.bind(this);
+    this.onChangedepartureAirport = this.onChangedepartureAirport.bind(this);
+    this.onChangearrivalAirport = this.onChangearrivalAirport.bind(this);
+    this.onChangedeparturetime = this.onChangedeparturetime.bind(this);
+    this.onChangearrivaltime = this.onChangearrivaltime.bind(this);
+    this.onChangebaggageallowance= this.onChangebaggageallowance.bind(this);
+    this.onChangetripDuration = this.onChangetripDuration.bind(this);
+    this.onChangeprice = this.onChangeprice.bind(this);
+    this.onSubmit=this.onSubmit.bind(this)
     this.state = { 
       flightnumber: 0,
       firstclass: 0,
@@ -23,53 +26,96 @@ export default class CreateFlight extends Component {
       arrivalAirport: '',
       departuretime : '',
       arrivaltime: '',
-      date:'',
       baggageallowance : 0,
       tripDuration : '',
       price: 0,
 
     }
   }
-  
- 
-  onChangeTime(e){
+  onChangeflightnumber(e){
     this.setState({
-        time:e.target.value
-    })
-   
-}
-onChangeDay(e){
-    this.setState({
-        day:e.target.value
+
+      flightnumber:e.target.value
     })
 }
+onChangefirstclass(e){
+  this.setState({
 
-  onChangeBuilding(e) {
-    this.setState({
-        building:e.target.value
-    })
-  }
-  onChangeRoom(e) {
-    this.setState({
-      room: e.target.value
-    })
-  }
- 
-  onChangeCrn(e){
-      this.setState({
+    firstclass:e.target.value
+  })
+}
+onChangeeconomy(e){
+  this.setState({
 
-        crn:e.target.value
-      })
-  }
- 
+    economy:e.target.value
+  })
+}
+onChangebusiness(e){
+  this.setState({
+
+    business:e.target.value
+  })
+}
+onChangedepartureAirport(e){
+  this.setState({
+
+    departureAirport:e.target.value
+  })
+}
+onChangearrivalAirport(e){
+  this.setState({
+
+    arrivalAirport:e.target.value
+  })
+}
+onChangedeparturetime(e){
+  this.setState({
+
+    departuretime:e.target.value
+  })
+}
+onChangearrivaltime(e){
+  this.setState({
+
+    arrivaltime:e.target.value
+  })
+}
+
+onChangebaggageallowance(e){
+  this.setState({
+
+    baggageallowance:e.target.value
+  })
+}
+onChangetripDuration(e){
+  this.setState({
+
+    tripDuration:e.target.value
+  })
+}
+onChangeprice(e){
+  this.setState({
+
+    price:e.target.value
+  })
+}
   onSubmit(e) {
     e.preventDefault();
-    this.state.array=this.props.match.params.name.split(",")
-const test ={
-    
+const test ={ 
+  flightnumber:this.state.flightnumber,
+  firstnumber:this.state.firstclass,
+  economy:this.state.economy,
+  business:this.state.business,
+  departureAirport:this.state.departureAirport,
+  arrivalAirport:this.state.arrivalAirport,
+  departuretime:this.state.departuretime,
+  arrivaltime:this.state.arrivaltime,
+  baggageallowance:this.state.baggageallowance,
+  tripDuration:this.state.tripDuration,
+  price:this.state.price
 }
 console.log(test)
-axios.post('http://localhost:5000/admins/addsections',test)
+axios.post('http://localhost:5000/admin/create-flight',test)
 .then(res=>{
 alert(res.data)
 
@@ -81,16 +127,16 @@ render() {
   var left = 350 + 'px';
   var top = 1+ 'px';
   var padding = 15 + 'px';
-  var right =0 +'px';
+  var right =600 +'px';
   var width = +'vh';
 
     return (
       
         <div style = {{width:"100vh"}}>
           <div class="ui compact message"  style={{color:'purple',padding,right,width, top,position:'fixed'}}>
-           <p>Fill out the form below to sign-up for a new account</p>
+           <p style={{color: 'purple',fontWeight: "900",fontstyle:'italic'}}>Fill out the form below to create a new flight</p>
          </div>
-        <form style = {{width:"100vh",position: 'absolute', left: '45%', top: '50%',
+        <form style = {{width:"100vh",position: 'absolute', left: '45%', top: '54%',
         transform: 'translate(-50%, -50%)'}} onSubmit={this.onSubmit}>
           
           <div className="form-group" > 
@@ -98,8 +144,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.firstclass}
+                onChange={this.onChangefirstclass}
                 className="form-control"
                
                 />
@@ -110,8 +156,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.economy}
+                onChange={this.onChangeeconomy}
                 className="form-control"
                
                 />
@@ -122,8 +168,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.business}
+                onChange={this.onChangebusiness}
                 className="form-control"
                
                 />
@@ -134,8 +180,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.departureAirport}
+                onChange={this.onChangedepartureAirport}
                 className="form-control"
                
                 />
@@ -146,20 +192,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
-                className="form-control"
-               
-                />
-          </div>
-            
-          <div className="form-group" > 
-            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}}>Departure date: </label>
-            <input  type="text"
-         
-                required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.arrivalAirport}
+                onChange={this.onChangearrivalAirport}
                 className="form-control"
                
                 />
@@ -170,8 +204,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.departuretime}
+                onChange={this.onChangedeparturetime}
                 className="form-control"
                
                 />
@@ -182,8 +216,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.arrivaltime}
+                onChange={this.onChangearrivaltime}
                 className="form-control"
                
                 />
@@ -194,8 +228,8 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.baggageallowance}
+                onChange={this.onChangebaggageallowance}
                 className="form-control"
                
                 />
@@ -206,30 +240,30 @@ render() {
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.tripDuration}
+                onChange={this.onChangetripDuration}
                 className="form-control"
                
                 />
           </div>
           <div className="form-group" > 
-            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}} >Trip duration: </label>
+            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}} >Flight number: </label>
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.flightnumber}
+                onChange={this.onChangeflightnumber}
                 className="form-control"
                
                 />
           </div>
           <div className="form-group" > 
-            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}} >Trip duration: </label>
+            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}} >Price: </label>
             <input  type="text"
          
                 required
-                value={this.state.time}
-                onChange={this.onChangeTime}
+                value={this.state.price}
+                onChange={this.onChangeprice}
                 className="form-control"
                
                 />
@@ -243,11 +277,11 @@ render() {
   </div>
   
         </form>
-        <Button  color='purple' content='Primary' animated  style = {{width:"17vh",position: 'absolute', left: '75%', top: '50%',
+        <Button onClick={this.onSubmit} color='purple' content='Primary' animated  style = {{width:"17vh",position: 'absolute', left: '75%', top: '50%',
         transform: 'translate(-50%, -50%)'}} type="submit" value="login" >
       <Button.Content visible>Submit</Button.Content>
       <Button.Content hidden>
-        <Icon name='plane' />
+        <Icon name='save' />
       </Button.Content>
     </Button> 
       </div>
