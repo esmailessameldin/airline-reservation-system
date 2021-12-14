ad3384b0516f4aacc86616d226ccab95f703dd8b
esmailessameldin <somaasom3a@gmail.com>
2021-12-14 15:28:23 +0200

    fixed the last one

--- frontend/src/components/edit-flight.js
+++ frontend/src/components/edit-flight.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Button, Icon } from 'semantic-ui-react';
+import { Message } from 'semantic-ui-react';
 import axios from 'axios';
-
+import firstimage from './firstclass.jpg'
+import economyimage from './economy.jpg'
+import businessimage from './buisnessclass.jpg'
+import { Text } from "react-native";
 export default class editflight extends Component {
   constructor(props) {
     super(props);
     this.onChangeretseat = this.onChangeretseat.bind(this);
+    this.onClick=this.onClick.bind(this)
+    this.onClick2=this.onClick2.bind(this)
+    this.onClick3=this.onClick3.bind(this)
     this.onSubmit=this.onSubmit.bind(this)
     this.state = { 
         username:"",
@@ -13,11 +20,49 @@ export default class editflight extends Component {
         index:1,
         retseat:'',
         retflightnum:0,
-        newseat:[],
-        newseat2:''
+        newseat:'',
+        newseat2:'',
+        flight:[],
+        cabin:[],
+        seatpicked:false,
+        freeseatsFirst:0,
+        freeseatsbus:0,
+        freeseatseco:0,
     }
   }
+  onClick(e){
+    e.preventDefault();
+    this.setState({
+      retseat:this.state.newseat+'0 ',
+       containerpicked:true,
+      flightpicked:false,
+      class:0,
+      seatpicked:true
+    })
+  
+  }
+  onClick2(e){
+    e.preventDefault();
+    this.setState({
+      retseat:this.state.newseat+'1 ',
+      containerpicked:true,
+      flightpicked:false,
+      class:1,
+      seatpicked:true,
+   })
+
+  }
+  onClick3(e){
+    e.preventDefault();
+    this.setState({
+      retseat:this.state.newseat+'2 ',
+      containerpicked:true,
+      flightpicked:false,
+      class:2,
+      seatpicked:true
+   })
  
+  }
 
 onChangeretseat(e){
   this.setState({
@@ -34,8 +79,9 @@ this.state.newseat2=this.state.newseat2+"2 "+this.state.newseat[1]
 
 }
 
-  onSubmit(e) {
+  onSubmit(e,i) {
     e.preventDefault();
+   this.state.retseat=this.state.retseat+i
 const test ={ 
  username:this.props.location.state.username,
  oldseat:this.props.location.state.flightseat,
@@ -56,21 +102,48 @@ alert(res.data)
   }
 
   componentDidMount(){
-      console.log(this.props.location.state)
-    const test={
-      name:this.props.match.params.name
-    }
-    axios.post('http://localhost:5000/users/find-user',test).then(res=>{
-          this.setState({
+  
+   
+          
+            const flight={
+              number:this.props.location.state.number
+            }
+            axios.post('http://localhost:5000/admin/findlflight ',flight).then(res=>{
+
+         
+            this.setState({
+              selectedflight:res.data[0]
+            })
+            console.log(res.data[0])
+
+
+            
+            this.setState({
            
-            firstName:res.data.firstName,
-            lastName:res.data.lastName,
-            passportNumber:res.data.passportNumber,
-            Email:res.data.Email,
-          })
-  
-    })
-  
+              cabin:this.state.selectedflight.cabin
+              
+            })
+           
+            for(var i=0;i<this.state.cabin[0].seats.length;i++){
+              if(!this.state.cabin[0].seats[i]){
+                   this.state.freeseatsFirst=this.state.freeseatsFirst+1
+              }
+            }
+            for(var i=0;i<this.state.cabin[1].seats.length;i++){
+              if(!this.state.cabin[1].seats[i]){
+                   this.state.freeseatseco=this.state.freeseatseco+1
+              }
+            }
+            for(var i=0;i<this.state.cabin[2].seats.length;i++){
+              if(!this.state.cabin[2].seats[i]){
+                   this.state.freeseatsbus=this.state.freeseatsbus+1
+              }
+            }
+            this.setState({
+              flightpicked:true})
+            console.log(this.state)
+            })
+
   }
   
 
@@ -83,45 +156,117 @@ render() {
   var padding = 15 + 'px';
   var right =600 +'px';
   var width = +'vh';
-
-    return (
-      
-        <div style = {{width:"100vh"}}>
-          <div class="ui compact message"  style={{color:'purple',padding,right,width, top,position:'fixed'}}>
-           <p style={{color: 'purple',fontWeight: "900",fontstyle:'italic'}}>Enter the new seat you desire to book</p>
-           
-         </div>
-        <form style = {{width:"100vh",position: 'absolute', left: '45%', top: '40%',
-        transform: 'translate(-50%, -50%)'}} onSubmit={this.onSubmit}>
-          <div className="form-group" > 
-            <label style={{color: 'purple',backgroundColor: 'teal',fontWeight: "900",fontstyle:'italic'}} >New seat: </label>
-            <input  type="text"
-         
-                required
-                value={this.state.retseat}
-                onChange={this.onChangeretseat}
-                className="form-control"
-               
-                />
-          </div>
-      
-          <div>
-         
-  
-  </div>
-  
-        </form>
-        <Button onClick={this.onSubmit} color='purple' content='Primary' animated  style = {{width:"17vh",position: 'absolute', left: '50%', top: '70%',
-        transform: 'translate(-50%, -50%)'}} type="submit" value="login" >
-      <Button.Content visible>Submit</Button.Content>
-      <Button.Content hidden>
-        <Icon name='save' />
-      </Button.Content>
-    </Button> 
-
+  var imagestyle={
+             
+    width: 35+'vh',
+    height: 25+'vh',
+    left: '5%', 
+    top: '40%',
+    position: 'absolute'
+}
+var imagestyle2={
    
+  width: 35+'vh',
+  height: 25+'vh',
+  left: '40%', 
+  top: '40%',
+  position: 'absolute'
+}
+var imagestyle3={
+   
+width: 35+'vh',
+height: 25+'vh',
+left: '73%', 
+top: '40%',
+position: 'absolute'
+}
+var textstyle={
+  fontSize: 13,
+  fontWeight: "bold",
+  left: '3%', 
+  top: '37%',
+  position: 'absolute',color: 'purple',fontstyle:'oldstyle-nums' ,backgroundColor:"teal"
+}
+var textstyle2={
+  fontSize: 13,
+  fontWeight: "bold",
+  left: '35%', 
+  top: '37%',
+  position: 'absolute',color: 'purple',fontstyle:'oldstyle-nums' ,backgroundColor:"teal"
+}
+var textstyle3={
+  fontSize: 13,
+  fontWeight: "bold",
+  left: '68%', 
+  top: '37%',
+  position: 'absolute',color: 'purple',fontstyle:'oldstyle-nums' ,backgroundColor:"teal"
+}
+if(!this.state.seatpicked)
+  return(<div>
+    <Message size='small' color='purple'  style={{right:'30%',width:'70vh', top:'5%',position:'fixed'}}
+  icon='user'
+  header={"Please choose the preferred class for the return flight"}
+  
+  />
+  <Text style={textstyle}>This is the first class cabin and there is {this.state.freeseatsFirst} free seats</Text>
+  <Text style={textstyle2}>This is the economy class cabin and there is {this.state.freeseatsbus} free seats</Text>
+  <Text style={textstyle3}>This is the business class cabin and there is {this.state.freeseatseco} free seats</Text>
+  <img class="ui small image" src={firstimage} style = {imagestyle} />
+  <img class="ui small image" src={economyimage} style = {imagestyle2} />
+  <img class="ui small image" src={businessimage} style = {imagestyle3} />
+  <Button  color='purple' onClick={this.onClick} content='Primary' animated  style = {{position: 'absolute', left: '14%', top: '70%',
+  transform: 'translate(-50%, -50%)'}}  >
+  <Button.Content visible>First class</Button.Content>
+  <Button.Content hidden>
+  <Icon name="hand point right outline" />
+  </Button.Content>
+  </Button><Button  color='purple' onClick={this.onClick2} content='Primary' animated  style = {{position: 'absolute', left: '49%', top: '70%',
+  transform: 'translate(-50%, -50%)'}}  >
+  <Button.Content visible>Economy class </Button.Content>
+  <Button.Content hidden>
+  <Icon name="hand point right outline" />
+  </Button.Content>
+  </Button><Button  color='purple' onClick={this.onClick3} content='Primary' animated  style = {{position: 'absolute', left: '82%', top: '70%',
+  transform: 'translate(-50%, -50%)'}}  >
+  <Button.Content visible>Buisness class</Button.Content>
+  <Button.Content hidden>
+  <Icon name="hand point right outline" />
+  </Button.Content>
+  </Button>
+  
+  
+  
+  
+  </div>)
+if(this.state.seatpicked)
+    return (
+      <div class="flex-container">
+      {this.state.cabin[this.state.class].seats.map((item, key) => {
+        if(!item)
+     return (
+      <div key={key}>
+<button  onClick={(e) => this.onSubmit(e,key)} class={"ui compact green labeled icon button"} >
+<i class="plane icon"></i>Seat: {key+1}
+</button>
+    </div>
+    
+    )
+    else{
+      return(
+        <button key={key} class="ui red disabled button">
+<i class="ban icon"></i>
+Seat {key+1} is taken
+</button>
 
-      </div>
+      )
+    }
+   })}
+        
+      
+
+    
+    </div>
+
     )
   }
 }
